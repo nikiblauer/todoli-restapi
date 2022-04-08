3cf45ed3b754f094ec237563cb17607f92b51c03
Niklas Gruener <niki.gruener@gmail.com>
2022-04-08 08:10:51 +0200

    added port env

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3wgf3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log("Server listening on port: ", process.env.PORT);
     });
   })
